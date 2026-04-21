56eaa0cc2fa77016302450342e14e44268741895
Bruno <brunuslife@gmail.com>
2026-04-20 21:05:45 -0300

    fix: atualiza mensagem whatsapp do telefone fixo

--- src/data/contactData.ts
+++ src/data/contactData.ts
@@ -6,7 +6,7 @@ export const contactData = {
     display: "(47) 99608-9929",
   },
   phone: {
-    link: "https://wa.me/554735149039?text=Ol%C3%A1%2C%20vim%20do%20site%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.",
+    link: "https://wa.me/554735149039?text=Ol%C3%A1!%20Tudo%20bem%3F%20%F0%9F%98%8A%0A%0AVi%20o%20site%20da%20Cont%C3%A9m%20Mais%20Lucro%20e%20me%20interessei%20pelos%20servi%C3%A7os%20de%20voc%C3%AAs.%20Gostaria%20de%20entender%20melhor%20como%20podem%20me%20ajudar.%0A%0APodemos%20conversar%20por%20aqui%3F",
     display: "(47) 3514-9039",
   },
   email: "contato@contemaislucro.com.br",
